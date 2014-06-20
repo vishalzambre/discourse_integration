b85a5b14c037bad92179b0c4b655ef00f4c35024
zambrevishal <vishal.zambre@weboniselab.com>
2014-06-20 19:19:24 +0530

    few changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ require 'base64'
 # home controller
 class HomeController < ApplicationController
   def index
-    group = [13,14]
-    user = 'webonise4@webonise.com'
+    group = "11,12"
+    user = 'webonise001@webonise.com'
     ts = Time.now.to_i
     secret = 'e47d33919f45f0501595f996bc6b09552ed5cc56f0f68051bc743ea32ad7ffd0'
     @sso = Base64.encode64("#{user}:#{group}:#{ts}:#{secret}")
